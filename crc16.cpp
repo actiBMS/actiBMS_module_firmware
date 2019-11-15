0df74b4312758c257ebfbe365967261f834823a5
hitech95 <nicveronese@gmail.com>
2019-11-15 02:25:20 +0100

    CRC: Removed unused function

--- crc16.cpp
+++ crc16.cpp
@@ -4,8 +4,6 @@
 uint16_t CRC16::CalculateArray(uint8_t data[], uint16_t length)
 {
   //Calculates XMODEM CRC16
-  uint8_t reflectIn = false;
-  uint8_t reflectOut = false;
   uint16_t polynomial = 0x1021;
   uint16_t xorIn = 0x0000;
   uint16_t xorOut = 0x0000;
@@ -23,9 +21,6 @@ uint16_t CRC16::CalculateArray(uint8_t data[], uint16_t length)
   for (uint16_t i = 0; i < length; i++)
   {
     c = data[i];
-
-    if (reflectIn != 0)
-      c = (uint8_t) reflect(c, 8);
 
     j = 0x80;
 
@@ -48,25 +43,5 @@ uint16_t CRC16::CalculateArray(uint8_t data[], uint16_t length)
     }
   }
 
-  if (reflectOut != 0)
-    crc = (uint16_t)((reflect(crc) ^ xorOut) & mask);
-
   return crc;
-}
-
-uint8_t CRC16::reflect(uint8_t data, uint8_t bits)
-{
-  unsigned long reflection = 0x00000000;
-
-  for (uint8_t bit = 0; bit < bits; bit++)
-  {
-    if ((data & 0x01) != 0)
-    {
-      reflection |= (unsigned long)(1 << ((bits - 1) - bit));
-    }
-
-    data = (uint8_t)(data >> 1);
-  }
-
-  return reflection;
 }
